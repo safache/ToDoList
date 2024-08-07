634f8e8b9ece4200b6ae3f3e7caa6933b218c833
safache <safa10.cherif@gmail.com>
2024-08-07 13:35:11 +0200

    version3

--- listeJs.js
+++ listeJs.js
@@ -1,42 +1,44 @@
 
 
 /*************************************addTask************************************** */
- document.getElementById("taskInput").addEventListener("keydown", function(event) {
-      if (event.key === "Enter") {
-        addTask();
-      }
+/*document.addEventListener("DOMContentLoaded", function() {*/
+    function addTask() { 
+        var input = document.getElementById("taskInput").value; 
+        var ul = document.getElementById("taskList"); 
+
+        if (input == "") {
+            alert("Please enter a task.");
+        } else {
+            let li = document.createElement("li");
+            li.innerHTML = '<i class="fa-regular fa-circle check-icon" ></i> '+input+'<i class="fa-regular fa-pen-to-square edit-icon"></i><i class="fa-regular fa-trash-can trash-icon"></i>';
+            
+            let checkIcon = li.querySelector(".check-icon");
+            checkIcon.addEventListener("click", function() {
+                toggleCheckIcon(li, input);
+            });
+
+            let editIcon = li.querySelector(".edit-icon");
+            editIcon.addEventListener("click", function() {
+                editTask(li, input);
+            });
+
+            let deleteIcon = li.querySelector(".trash-icon");
+            deleteIcon.addEventListener("click", function() {
+                deleteTask(li);
+            });
+            ul.appendChild(li); 
+            document.getElementById("taskInput").value = ''; 
+        }
+    }
+
+    var input = document.getElementById("taskInput"); 
+    input.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            addTask();
+        }
     });
-function addTask() { 
-    var input = document.getElementById("taskInput").value; 
-    var ul = document.getElementById("taskList"); 
-    
-    if (input=="") {
-      alert("Please enter a task.");
-    } else {
-      let li = document.createElement("li");
-      li.innerHTML = '<i class="fa-regular fa-circle check-icon" ></i> '+input+'<i class="fa-regular fa-pen-to-square edit-icon"></i><i class="fa-regular fa-trash-can trash-icon"></i>';
-      
-      let checkIcon = li.querySelector(".check-icon");
-      checkIcon.addEventListener("click", function() {
-        toggleCheckIcon(li, input);
-      });
-  
-      let editIcon = li.querySelector(".edit-icon");
-      editIcon.addEventListener("click", function() {
-        editTask(li, input);
-      });
-      
-      let deleteIcon = li.querySelector(".trash-icon");
-        deleteIcon.addEventListener("click", function() {
-          deleteTask(li);
-        });
-    ul.appendChild(li); 
-      document.getElementById("taskInput").value = ''; 
-    }
-     
-    }
-  
-    
+
+
     /*************************************toggle************************************** */
 
     
